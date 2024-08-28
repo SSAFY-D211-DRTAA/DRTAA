9c4e9ced139db4c1c857a230b95a9e74ce8f89a6
이현진 <dlguswls456@naver.com>
2024-08-28 23:50:29 +0900

    chore: SubProject1 6까지 완료

---
+++ 이현진/catkin_ws/build/skeleton/ssafy_ad/ssafy_1/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/opt/ros/noetic/include;/usr/include;/home/jeongjae/catkin_ws/devel/include".split(';') if "/opt/ros/noetic/include;/usr/include;/home/jeongjae/catkin_ws/devel/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "message_runtime".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "/opt/ros/noetic/lib/libroscpp_serialization.so;/opt/ros/noetic/lib/librostime.so;/usr/lib/x86_64-linux-gnu/libboost_date_time.so.1.71.0;/opt/ros/noetic/lib/libcpp_common.so;/usr/lib/x86_64-linux-gnu/libboost_system.so.1.71.0;/usr/lib/x86_64-linux-gnu/libboost_thread.so.1.71.0;/usr/lib/x86_64-linux-gnu/libconsole_bridge.so.0.4".split(';') if "/opt/ros/noetic/lib/libroscpp_serialization.so;/opt/ros/noetic/lib/librostime.so;/usr/lib/x86_64-linux-gnu/libboost_date_time.so.1.71.0;/opt/ros/noetic/lib/libcpp_common.so;/usr/lib/x86_64-linux-gnu/libboost_system.so.1.71.0;/usr/lib/x86_64-linux-gnu/libboost_thread.so.1.71.0;/usr/lib/x86_64-linux-gnu/libconsole_bridge.so.0.4" != "" else []
+PROJECT_NAME = "ssafy_1"
+PROJECT_SPACE_DIR = "/home/jeongjae/catkin_ws/devel"
+PROJECT_VERSION = "0.0.0"
