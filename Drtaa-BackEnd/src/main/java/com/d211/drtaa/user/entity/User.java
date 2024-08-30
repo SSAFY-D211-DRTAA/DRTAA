51b4ce8a5b8825c54758d05fa1b52db3e5ae0332
윤가희 <yunkh2005@naver.com>
2024-08-30 13:31:44 +0900

    Add: BackEnd 프로젝트
    서버 최초 생성

---
+++ Drtaa-BackEnd/src/main/java/com/d211/drtaa/user/entity/User.java
@@ -0,0 +1,76 @@
+package com.d211.drtaa.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private long userId;
+
+    @Column(name = "user_email", nullable = false)
+    private String userEmail;
+
+    @Column(name = "user_password", nullable = false)
+    private String userPassword;
+
+    @Column(name = "user_nickname", nullable = false)
+    private String userNickname;
+
+    @Column(name = "user_refreshtoken", nullable = false)
+    private String userRefreshToken;
+
+    @Column(name = "user_is_admin", nullable = false)
+    private boolean userIsAdmin;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(userIsAdmin ? "ROLE_ADMIN" : "ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return userPassword;
+    }
+
+    @Override
+    public String getUsername() {
+        return userEmail;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ Drtaa-BackEnd/src/test/java/com/d211/drtaa/DrtaaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d211.drtaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrtaaBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
