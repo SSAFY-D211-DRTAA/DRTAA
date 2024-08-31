b43598686d66f3d27ad38e2990f043a98fbe9556
윤가희 <yunkh2005@naver.com>
2024-08-31 17:01:33 +0900

    refactor: 반환타입
    ResponseEntity 반환 타입 수정

--- Drtaa-BackEnd/src/main/java/com/d211/drtaa/user/controller/UserController.java
+++ Drtaa-BackEnd/src/main/java/com/d211/drtaa/user/controller/UserController.java
@@ -35,73 +35,54 @@ public class UserController {
     @PostMapping("/signup")
     @Operation(summary = "회원가입", description = "Form 회원가입")
     public ResponseEntity<?> signUp(@RequestBody SignUpRequestDTO request) {
-        HttpStatus status = HttpStatus.ACCEPTED; // 202
-
         try {
             userDetailsService.createUser(request);
-            status = HttpStatus.CREATED; // 201, 요청이 성공적으로 처리 & 새로운 리소스가 생성
 
-            return new ResponseEntity<>(status);
-        }catch (DataIntegrityViolationException e) {
-            status = HttpStatus.CONFLICT; // 409, 리소스 간의 충돌이 발생
-
-            return new ResponseEntity<>(e.getMessage(), status);
+            // 회원가입 성공 응답 (200 OK)
+            return ResponseEntity.ok("회원가입 성공");
+        } catch (DataIntegrityViolationException e) {
+            // 409, 리소스 간의 충돌이 발생했을 때
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
         } catch (UserCreationException e) {
-            status = HttpStatus.BAD_REQUEST; // 400, 클라이언트의 요청이 잘못됨
-
-            return new ResponseEntity<>(e.getMessage(), status);
-
+            // 400, 잘못된 요청에 대한 예외 처리
+            return ResponseEntity.badRequest().body(e.getMessage());
         } catch (Exception e) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR; // 500, 서버 오류
-
-            return new ResponseEntity<>(e.getMessage(), status);
+            // 500, 서버 오류가 발생했을 때
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
 
     @PostMapping("/login")
     @Operation(summary = "로그인", description = "Form 로그인")
     public ResponseEntity<?> login(@RequestBody FormLoginRequestDTO request) {
-        HttpStatus status = HttpStatus.ACCEPTED; // 202
-
         try {
             JwtToken tokens = userService.login(request);
-            status = HttpStatus.OK; // 200, 클라이언트 요청 성공
 
-            return new ResponseEntity<>(tokens, status);
-
+            // 200, 클라이언트 요청 성공
+            return ResponseEntity.ok(tokens);
         } catch(UsernameNotFoundException e) {
-            status = HttpStatus.NOT_FOUND; // 404, 클라이언트 요청 탐색 실패
-
-            return new ResponseEntity<>(e.getMessage(), status);
+            // 404, 클라이언트 요청 탐색 실패
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         } catch (Exception e) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR; // 500, 서버 오류
-
-            return new ResponseEntity<>(e.getMessage(), status);
+            // 400, 잘못된 요청
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
     @GetMapping("/info")
     @Operation(summary = "회원 정보 조회", description = "액세스 토큰을 사용해 회원 정보 조회")
     public ResponseEntity<?> info(Authentication authentication) {
-        HttpStatus status = HttpStatus.ACCEPTED;
-
         try {
             UserInfoResponseDTO response = userService.info(authentication.getName());
 
-            status = HttpStatus.OK; // 200, 클라이언트 요청 성공
-
-            return new ResponseEntity<>(response, status);
+            // 200, 클라이언트 요청 성공
+            return ResponseEntity.ok(response);
         } catch (UsernameNotFoundException e) {
-            status = HttpStatus.NOT_FOUND; // 404, 클라이언트 요청 탐색 실패
-
-            return new ResponseEntity<>(e.getMessage(), status);
+            // 404, 클라이언트 요청 탐색 실패
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         } catch (Exception e) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR; // 500, 서버 오류
-
-            return new ResponseEntity<>(e.getMessage(), status);
+            // 400, 잘못된 요청
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
-
-
-
 }
