40a345748a69ffff0ca57d396cbb1c94b6bc8a16
HYUNJIN LEE <dlguswls456@naver.com>
2024-09-02 23:52:47 +0900

    feat: 네이버 로그인 아주 간단 구현

--- Drtaa-Android/feature-sign/src/main/java/com/drtaa/feature_sign/SignActivity.kt
+++ Drtaa-Android/feature-sign/src/main/java/com/drtaa/feature_sign/SignActivity.kt
@@ -1,17 +1,22 @@
 package com.drtaa.feature_sign
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.drtaa.core_ui.base.BaseActivity
 import com.drtaa.feature_sign.databinding.ActivitySignBinding
+import com.navercorp.nid.NaverIdLoginSDK
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SignActivity : BaseActivity<ActivitySignBinding>(R.layout.activity_sign) {
     override fun init() {
+        initNaverSign()
+    }
 
+    private fun initNaverSign(){
+        NaverIdLoginSDK.initialize(
+            this,
+            "",
+            "",
+            "사용자",
+        )
     }
 }
--- Drtaa-Android/feature-sign/src/main/java/com/drtaa/feature_sign/SignInFragment.kt
+++ Drtaa-Android/feature-sign/src/main/java/com/drtaa/feature_sign/SignInFragment.kt
@@ -9,7 +9,10 @@ import android.view.ViewGroup
 import com.drtaa.core_ui.base.BaseFragment
 import com.drtaa.feature_main.MainActivity
 import com.drtaa.feature_sign.databinding.FragmentSignInBinding
+import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.oauth.OAuthLoginCallback
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class SignInFragment : BaseFragment<FragmentSignInBinding>(R.layout.fragment_sign_in) {
@@ -22,6 +25,31 @@ class SignInFragment : BaseFragment<FragmentSignInBinding>(R.layout.fragment_sig
         binding.signinMoveToMainBtn.setOnClickListener {
             startActivity(Intent(requireContext(), MainActivity::class.java))
             requireActivity().finish()
+        }
+
+        binding.signinNaverBtn.setOnClickListener {
+            NaverIdLoginSDK.authenticate(requireActivity(), oAuthLoginCallback)
+        }
+    }
+
+    val oAuthLoginCallback = object : OAuthLoginCallback {
+        override fun onError(errorCode: Int, message: String) {
+            onFailure(errorCode, message)
+        }
+
+        override fun onFailure(httpStatus: Int, message: String) {
+            val errorCode = NaverIdLoginSDK.getLastErrorCode().code
+            val errorDescription = NaverIdLoginSDK.getLastErrorDescription()
+            Timber.tag("fail naver login").d("error code $errorCode, error description $errorDescription")
+        }
+
+        override fun onSuccess() {
+            Timber.tag("success naver login").d("AccessToken : ${NaverIdLoginSDK.getAccessToken()}")
+            Timber.tag("success naver login").d("ReFreshToken : ${NaverIdLoginSDK.getRefreshToken()}")
+            Timber.tag("success naver login").d("Expires  : ${NaverIdLoginSDK.getExpiresAt()}")
+            Timber.tag("success naver login").d("TokenType   : ${NaverIdLoginSDK.getTokenType()}")
+            Timber.tag("success naver login").d("State   : ${NaverIdLoginSDK.getState()}")
+
         }
     }
 
