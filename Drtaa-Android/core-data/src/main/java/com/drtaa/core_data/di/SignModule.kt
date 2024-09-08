88f08a2e55ef229382c0233ee7900ff9c28dd581
HYUNJIN LEE <dlguswls456@naver.com>
2024-09-08 18:41:51 +0900

    chore: detekt 수정

--- Drtaa-Android/core-data/src/main/java/com/drtaa/core_data/di/SignModule.kt
+++ Drtaa-Android/core-data/src/main/java/com/drtaa/core_data/di/SignModule.kt
@@ -1,13 +1,10 @@
 package com.drtaa.core_data.di
 
-import android.content.Context
-import androidx.credentials.CredentialManager
 import com.drtaa.core.BuildConfig
 import com.google.android.libraries.identity.googleid.GetGoogleIdOption
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
