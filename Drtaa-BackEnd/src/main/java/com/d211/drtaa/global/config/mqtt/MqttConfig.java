a15b534e4f13faf29b5e6c352ad905d372a43ffe
최승준 <tmdwns63@gmail.com>
2024-09-11 15:56:41 +0900

    feat: mqtt 통신 작업
    mqtt

---
+++ Drtaa-BackEnd/src/main/java/com/d211/drtaa/global/config/mqtt/MqttConfig.java
@@ -0,0 +1,46 @@
+package com.d211.drtaa.global.config.mqtt;
+
+
+import lombok.extern.log4j.Log4j2;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
+import org.springframework.messaging.MessageHandler;
+
+@Configuration
+@Log4j2
+@IntegrationComponentScan
+public class MqttConfig {
+    private static final String TAG = MqttConfig.class.getSimpleName();
+
+    @Value("${spring.mqtt.broken-url}")
+    private String brokenUrl;
+
+    @Value("${spring.mqtt.client-id}")
+    private String cliendId;
+
+    @Value("${spring.mqtt.topic}")
+    private String topic;
+
+    @Bean
+    public MqttPahoClientFactory mqttPahoClientFactory(){
+        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setServerURIs(new String[]{brokenUrl});
+        factory.setConnectionOptions(options);
+        return factory;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttOutboundChannel")
+    public MessageHandler mqttOutbound(){
+        MqttPahoMessageHandler messageHandler = new MqttPahoMessageHandler(cliendId,mqttPahoClientFactory());
+    }
+
+}
