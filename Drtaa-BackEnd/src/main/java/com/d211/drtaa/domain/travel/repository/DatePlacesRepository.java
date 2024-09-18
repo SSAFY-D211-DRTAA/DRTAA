b916407311a248273d3db36349ec3a5573d6fdcb
윤가희 <yunkh2005@naver.com>
2024-09-18 23:53:58 +0900

    add: travel관련 프로젝트 구조

---
+++ Drtaa-BackEnd/src/main/java/com/d211/drtaa/domain/travel/repository/DatePlacesRepository.java
@@ -0,0 +1,6 @@
+package com.d211.drtaa.domain.travel.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DatePlacesRepository extends JpaRepository<DatePlacesRepository, Long> {
+}
