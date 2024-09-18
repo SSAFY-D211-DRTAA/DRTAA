9cc7c896be63fad817fb1796369bd7c3e809b160
csjcozy <tmdwns63@gmail.com>
2024-09-18 15:07:19 +0900

    update: payment controller 구현 테스트

--- Drtaa-BackEnd/src/main/java/com/d211/drtaa/domain/payment/entity/Payment.java
+++ Drtaa-BackEnd/src/main/java/com/d211/drtaa/domain/payment/entity/Payment.java
@@ -17,46 +17,48 @@ import java.time.LocalDateTime;
 public class Payment {
 
     @Id
+    @Column(name = "id",nullable = false)
+    @Schema(description = "유저 번호", example = "1")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(name = "receipt_id", nullable = false)
     @Schema(description = "영수증 번호", example = "1")
     private String receiptId;
 
-    @Column(nullable = false)
+    @Column(name = "order_id", nullable = false)
     @Schema(description = "주문 번호", example = "1")
     private String orderId;
 
-    @Column(nullable = false)
+    @Column(name = "price", nullable = false)
     @Schema(description = "가격", example = "100")
     private Integer price;
 
-    @Column(nullable = false)
+    @Column(name = "payment_method", nullable = false)
     @Schema(description = "결제 방식", example = "카드")
     private String paymentMethod;
 
-    @Column(nullable = false)
+    @Column(name = "purchased_at", nullable = false)
     @Schema(description = "결제 시간", example = "date")
     private LocalDateTime purchasedAt;
 
-    @Column(nullable = false)
+    @Column(name = "user_id", nullable = false)
     @Schema(description = "유저 아이디", example = "1")
     private Long userId;
 
-    @Column(nullable = false)
+    @Column(name = "car_id", nullable = false)
     @Schema(description = "차 아이디", example = "1")
     private Long carId;
 
-    @Column(nullable = false)
+    @Column(name = "head_count", nullable = false)
     @Schema(description = "탑승 인원", example = "2")
     private Integer headCount;
 
-    @Column(nullable = false)
+    @Column(name = "rent_start_time", nullable = false)
     @Schema(description = "렌트 시작 시간", example = "date")
     private LocalDateTime rentStartTime;
 
-    @Column(nullable = false)
+    @Column(name = "rent_end_time", nullable = false)
     @Schema(description = "렌트 종료 시간", example = "date")
     private LocalDateTime rentEndTime;
 
