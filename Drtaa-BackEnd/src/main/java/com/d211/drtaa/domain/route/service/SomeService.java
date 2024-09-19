06acc0c8c3fc87711051e94bfef6d0536c689361
최승준 <tmdwns63@gmail.com>
2024-09-19 14:45:43 +0900

    feature: payment, mqtt주석

--- Drtaa-BackEnd/src/main/java/com/d211/drtaa/domain/route/service/SomeService.java
+++ Drtaa-BackEnd/src/main/java/com/d211/drtaa/domain/route/service/SomeService.java
@@ -1,18 +1,18 @@
-package com.d211.drtaa.domain.route.service;
-
-
-import com.d211.drtaa.domain.route.mqtt.Publisher;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class SomeService {
-    private static final String TAG = SomeService.class.getSimpleName();
-
-    private final Publisher publisher;
-
-    public void publishMessage(String topic, String message) {
-        publisher.publishLocations(topic, message);
-    }
-}
+//package com.d211.drtaa.domain.route.service;
+//
+//
+//import com.d211.drtaa.domain.route.mqtt.Publisher;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class SomeService {
+//    private static final String TAG = SomeService.class.getSimpleName();
+//
+//    private final Publisher publisher;
+//
+//    public void publishMessage(String topic, String message) {
+//        publisher.publishLocations(topic, message);
+//    }
+//}
