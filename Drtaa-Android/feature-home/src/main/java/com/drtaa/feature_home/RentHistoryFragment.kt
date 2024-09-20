c237e62213a3e99074cfe5fb95edf37e5c52db52
HYUNJIN LEE <dlguswls456@naver.com>
2024-09-20 14:08:49 +0900

    feat: 이용기록 리사이클러뷰 아이템 클릭이벤트 추가

--- Drtaa-Android/feature-home/src/main/java/com/drtaa/feature_home/RentHistoryFragment.kt
+++ Drtaa-Android/feature-home/src/main/java/com/drtaa/feature_home/RentHistoryFragment.kt
@@ -4,7 +4,9 @@ import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import com.drtaa.core_model.rent.RentSimple
 import com.drtaa.core_ui.base.BaseFragment
+import com.drtaa.core_ui.showSnackBar
 import com.drtaa.feature_home.adapter.RentHistoryAdapter
 import com.drtaa.feature_home.databinding.FragmentRentHistoryBinding
 import com.drtaa.feature_home.viewmodel.RentHistoryViewModel
@@ -38,6 +40,11 @@ class RentHistoryFragment :
     }
 
     private fun initRV() {
+        rentHistoryAdapter.setItemClickListener(object : RentHistoryAdapter.ItemClickListener {
+            override fun onItemClicked(rentSimple: RentSimple) {
+                showSnackBar("${rentSimple.rentId} 클릭이용")
+            }
+        })
         binding.rvRentHistory.adapter = rentHistoryAdapter
     }
 }
