e3d08793d7ad661cf00451afc8229f9b2b48711e
윤가희 <yunkh2005@naver.com>
2024-09-24 00:35:28 +0900

    fix: Spring Batch 관련

--- Drtaa-BackEnd/src/main/java/com/d211/drtaa/domain/rent/service/RentNotification.java
+++ Drtaa-BackEnd/src/main/java/com/d211/drtaa/domain/rent/service/RentNotification.java
@@ -1,45 +1,45 @@
-package com.d211.drtaa.domain.rent.service;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.PlatformTransactionManager;
-
-@Configuration
-@RequiredArgsConstructor
-@Log4j2
-public class RentNotification {
-    @Bean
-    public Job rentNotificationJob(JobRepository jobRepository, Step rentNotificationStep) {
-        log.info("RentNotification");
-
-        return new JobBuilder("rentNotification", jobRepository)
-                .start(rentNotificationStep)
-                .build();
-    }
-
-    @Bean
-    public Step rentNotificationStep(JobRepository jobRepository, Tasklet tasklet, PlatformTransactionManager transactionManager) {
-        log.info("RentNotificationStep");
-
-        return new StepBuilder("rentNotificationStep", jobRepository)
-                .tasklet(tasklet, transactionManager)
-                .build();
-    }
-
-    @Bean
-    public Tasklet tasklet() {
-        return ((contribution, chunkContext) -> {
-            log.info(">>>>> Tasklet");
-            return RepeatStatus.FINISHED;
-        });
-    }
-}
+//package com.d211.drtaa.domain.rent.service;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.log4j.Log4j2;
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.job.builder.JobBuilder;
+//import org.springframework.batch.core.repository.JobRepository;
+//import org.springframework.batch.core.step.builder.StepBuilder;
+//import org.springframework.batch.core.step.tasklet.Tasklet;
+//import org.springframework.batch.repeat.RepeatStatus;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.transaction.PlatformTransactionManager;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//@Log4j2
+//public class RentNotification {
+//    @Bean
+//    public Job rentNotificationJob(JobRepository jobRepository, Step rentNotificationStep) {
+//        log.info("RentNotification");
+//
+//        return new JobBuilder("rentNotification", jobRepository)
+//                .start(rentNotificationStep)
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step rentNotificationStep(JobRepository jobRepository, Tasklet tasklet, PlatformTransactionManager transactionManager) {
+//        log.info("RentNotificationStep");
+//
+//        return new StepBuilder("rentNotificationStep", jobRepository)
+//                .tasklet(tasklet, transactionManager)
+//                .build();
+//    }
+//
+//    @Bean
+//    public Tasklet tasklet() {
+//        return ((contribution, chunkContext) -> {
+//            log.info(">>>>> Tasklet");
+//            return RepeatStatus.FINISHED;
+//        });
+//    }
+//}
