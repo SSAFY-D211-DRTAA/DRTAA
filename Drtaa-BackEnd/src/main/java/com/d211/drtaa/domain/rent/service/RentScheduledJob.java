3e4a08a8b81e0e206185e7c5ec59264138499869
윤가희 <yunkh2005@naver.com>
2024-09-26 09:35:25 +0900

    comment: 테스트코드 처리

--- Drtaa-BackEnd/src/main/java/com/d211/drtaa/domain/rent/service/RentScheduledJob.java
+++ Drtaa-BackEnd/src/main/java/com/d211/drtaa/domain/rent/service/RentScheduledJob.java
@@ -16,15 +16,15 @@ public class RentScheduledJob {
     private final JobLauncher jobLauncher;
     private final Job rentNotificationJob;
 
-    @Scheduled(cron = "0 0/10 * * * *") // 매 10분마다 실행
-    public void checkEvery10Minutes() throws Exception {
-        log.info("10분마다 실행되는 메소드");
-    }
-
-    @Scheduled(cron = "0 0,30 * * * *") // 매 시간 0분과 30분에 실행
-    public void checkEvery30Minutes() throws Exception {
-        log.info("30분마다 실행되는 메소드");
-    }
+//    @Scheduled(cron = "0 0/10 * * * *") // 매 10분마다 실행
+//    public void checkEvery10Minutes() throws Exception {
+//        log.info("10분마다 실행되는 메소드");
+//    }
+//
+//    @Scheduled(cron = "0 0,30 * * * *") // 매 시간 0분과 30분에 실행
+//    public void checkEvery30Minutes() throws Exception {
+//        log.info("30분마다 실행되는 메소드");
+//    }
 
     @Scheduled(cron = "0 0 10 * * *") // 매일 오전 10시에 실행
     public void runRentNotificationJob() {
