af045a3abdd02919e3fe35d0c1130f0b6ba6a234
kimmandoo <mingyu5675@gmail.com>
2024-10-01 13:31:22 +0900

    feat: 앱 로고 추가, detekt 적용

--- Drtaa-Android/feature-sign/src/main/java/com/drtaa/feature_sign/SignActivity.kt
+++ Drtaa-Android/feature-sign/src/main/java/com/drtaa/feature_sign/SignActivity.kt
@@ -1,19 +1,11 @@
 package com.drtaa.feature_sign
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.lifecycle.flowWithLifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.NavHostFragment
 import com.drtaa.core_ui.base.BaseActivity
-import com.drtaa.feature_main.MainActivity
 import com.drtaa.feature_sign.databinding.ActivitySignBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import timber.log.Timber
 
 @AndroidEntryPoint
 class SignActivity : BaseActivity<ActivitySignBinding>(R.layout.activity_sign) {
