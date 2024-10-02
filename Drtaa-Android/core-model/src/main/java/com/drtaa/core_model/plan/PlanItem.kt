4f32521b53e28b6e01e08337f5c97212931a1ccb
HYUNJIN LEE <dlguswls456@naver.com>
2024-10-02 12:06:33 +0900

    chore: 마이페이지 닉네임, Serializable 대신 Parcelable 적용

--- Drtaa-Android/core-model/src/main/java/com/drtaa/core_model/plan/PlanItem.kt
+++ Drtaa-Android/core-model/src/main/java/com/drtaa/core_model/plan/PlanItem.kt
@@ -1,8 +1,8 @@
 package com.drtaa.core_model.plan
 
-import kotlinx.serialization.Serializable
+import android.os.Parcel
+import android.os.Parcelable
 
-@Serializable
 data class PlanItem(
     val travelDatesId: Int,
     val datePlacesAddress: String,
@@ -14,4 +14,44 @@ data class PlanItem(
     val datePlacesId: Int = 0,
     val datePlacesOrder: Int,
     var isSelected: Boolean = false
-) : java.io.Serializable
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readByte() != 0.toByte(),
+        parcel.readDouble(),
+        parcel.readDouble(),
+        parcel.readString() ?: "",
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readByte() != 0.toByte()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(travelDatesId)
+        parcel.writeString(datePlacesAddress)
+        parcel.writeString(datePlacesCategory)
+        parcel.writeByte(if (datePlacesIsVisited) 1 else 0)
+        parcel.writeDouble(datePlacesLat)
+        parcel.writeDouble(datePlacesLon)
+        parcel.writeString(datePlacesName)
+        parcel.writeInt(datePlacesId)
+        parcel.writeInt(datePlacesOrder)
+        parcel.writeByte(if (isSelected) 1 else 0)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<PlanItem> {
+        override fun createFromParcel(parcel: Parcel): PlanItem {
+            return PlanItem(parcel)
+        }
+
+        override fun newArray(size: Int): Array<PlanItem?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
