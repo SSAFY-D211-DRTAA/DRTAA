7abb004304d63eb75d38e5faf996e64c244d211c
윤가희 <yunkh2005@naver.com>
2024-10-02 10:22:03 +0900

    fix: 여행 상세 조회

--- Drtaa-BackEnd/src/main/java/com/d211/drtaa/domain/travel/repository/DatePlacesRepository.java
+++ Drtaa-BackEnd/src/main/java/com/d211/drtaa/domain/travel/repository/DatePlacesRepository.java
@@ -13,8 +13,7 @@ import java.util.Optional;
 public interface DatePlacesRepository extends JpaRepository<DatePlaces, Long> {
     // find
     Optional<DatePlaces> findByDatePlacesId(Long datePlacesId);
-    @Query(value = "SELECT * FROM date_places WHERE travelDatesId = :travelDatesId", nativeQuery = true)
-    List<DatePlaces> findByTravelDatesId(@Param("travelDatesId") Long travelDatesId);
+    List<DatePlaces> findByTravelDates(TravelDates travelDates);
     @Query("SELECT dp FROM DatePlaces dp WHERE dp.travelDates.travelDatesId = :travelDatesId ORDER BY dp.datePlacesOrder DESC")
     Optional<DatePlaces> findLastPlaceByTravelDatesId(@Param("travelDatesId") Long travelDatesId);
     List<DatePlaces> findByTravelDatesAndDatePlacesOrderGreaterThan(TravelDates date, int datePlacesOrder);
