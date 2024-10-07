a665073cfc50b4f49638c7f3fd886e13369e7352
Mingyu Kim <mingyu5675@gmail.com>
2024-10-07 17:50:24 +0900

    feat: intent 넣기

--- Drtaa-Android/feature-main/src/main/java/com/drtaa/feature_main/DrtaaMessagingService.kt
+++ Drtaa-Android/feature-main/src/main/java/com/drtaa/feature_main/DrtaaMessagingService.kt
@@ -4,6 +4,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import androidx.core.app.NotificationCompat
 import com.drtaa.core_model.map.Search
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -21,26 +22,53 @@ class DrtaaMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+        message.data.let {
+            val datePlacesName = it["datePlacesName"]
+            val datePlacesCategory = it["datePlacesCategory"]
+            val datePlacesAddress = it["datePlacesAddress"]
+            val datePlacesLat = it["datePlacesLat"]?.toDouble()
+            val datePlacesLon = it["datePlacesLon"]?.toDouble()
+
+            val request = Search(
+                title = datePlacesName.toString(),
+                category = datePlacesCategory.toString(),
+                roadAddress = datePlacesAddress.toString(),
+                lng = datePlacesLat ?: 0.0,
+                lat = datePlacesLon ?: 0.0
+            )
+
+            val mainIntent = Intent(this, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                putExtra("datePlacesName", datePlacesName)
+                putExtra("datePlacesCategory", datePlacesCategory)
+                putExtra("datePlacesAddress", datePlacesAddress)
+                putExtra("datePlacesLat", datePlacesLat)
+                putExtra("datePlacesLon", datePlacesLon)
+            }
+
+            val pIntent: PendingIntent =
+                PendingIntent.getActivity(this, 0, mainIntent, PendingIntent.FLAG_IMMUTABLE)
+
+            val builder = NotificationCompat.Builder(this, MainActivity.CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher) // 반드시 작은 아이콘을 설정
+                .setContentIntent(pIntent)
+                .setContentTitle("여행지를 추천합니다!")
+                .setContentText("현재 위치를 기준으로 추천합니다.")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            notificationManager.notify(MainActivity.NOTIFICATION_ID, builder.build())
+
+            Timber.tag("fcm").d("place  recomm $request")
+        }
+
+
         message.notification?.let { msg ->
             val title = msg.title
             val body = msg.body
-            message.data.let {
-                val datePlacesName = it["datePlacesName"]
-                val test = it["test"]
-                val datePlacesCategory = it["datePlacesCategory"]
-                val datePlacesAddress = it["datePlacesAddress"]
-                val datePlacesLat = it["datePlacesLat"]?.toDouble()
-                val datePlacesLon = it["datePlacesLon"]?.toDouble()
-
-                val request = Search(
-                    title = datePlacesName.toString(),
-                    category = datePlacesCategory.toString(),
-                    roadAddress = datePlacesAddress.toString(),
-                    lng = datePlacesLat ?: 0.0,
-                    lat = datePlacesLon ?: 0.0
-                )
-                Timber.tag("fcm").d("place $test  recomm $request")
-            }
 
             val mainIntent = Intent(this, MainActivity::class.java).apply {
                 flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
@@ -50,7 +78,7 @@ class DrtaaMessagingService : FirebaseMessagingService() {
                 PendingIntent.getActivity(this, 0, mainIntent, PendingIntent.FLAG_IMMUTABLE)
 
             val builder = NotificationCompat.Builder(this, MainActivity.CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_foreground) // 반드시 작은 아이콘을 설정
+                .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentIntent(pIntent)
                 .setContentTitle(title)
                 .setContentText(body)
